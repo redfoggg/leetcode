dded350eb08e4e5df5a3e0d5a9e85f4a94637244
David Ferrari Iteris <david.ferrari.iteris@david.ferrari.iteris>
2022-05-08 00:38:31 -0300

    Some leet-code interview questions

---
+++ reverse-integer/reverse-integer-csharp/Program.cs
@@ -0,0 +1,20 @@
+﻿public class Solution {
+    public int Reverse(int x) 
+    {
+        int reverse = 0;
+        int maxInt = int.MaxValue;
+        int minInt = int.MinValue;
+        while(x != 0)
+        {
+            var lastDigit = x % 10;
+
+            if(reverse > maxInt/10 || (reverse == maxInt/10 && lastDigit > 7)) return 0;
+            if(reverse < minInt/10 || (reverse == minInt/10 && lastDigit < -8)) return 0;
+            
+            reverse = (reverse * 10) + lastDigit;
+            x /= 10;
+        }
+
+        return reverse;
+    }
+}
---
+++ reverse-string/reverse-string-csharp/Program.cs
@@ -0,0 +1,22 @@
+﻿// Time complexity O(n), Extra space O(1)
+// Interview problems - Easy collection
+public class Solution {
+    public void ReverseString(char[] s) {
+        int start = 0;
+        int end = s.Length - 1;
+        while(start < end)
+        {
+            s = Swap(s, start, end);
+            start++;
+            end--;
+        }
+    }
+    
+    static char[] Swap(char[] ch, int i, int j)
+    {
+        char temp = ch[i];
+        ch[i] = ch[j];
+        ch[j] = temp;
+        return ch;
+    }
+}
